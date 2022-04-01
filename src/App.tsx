c2fad04d15976f761bf6aef9891deba3ee211c0e
Antonov Kirill <antonovkir2@gmail.com>
2022-04-01 11:21:15 +0500

    new task unit 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 import React, {useState} from 'react';
 import './App.css';
-import OnOff from "./components/OnOff";
-import {Rating, RatingValueType} from './components/Rating';
-import {Accordion} from './components/Accordion';
-
+import {Rating} from "./components/Rating/Rating";
+import {RatingType, UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
+import {Accordion} from "./components/Accordion/Accordion";
+import {UncontrolledAccordion} from "./components/UncontrolledAccordion/UncontrolledAccordion";
+import {UncontrolledOnOff} from "./components/UncontrolledOnOff/UncontrolledOnOff";
+import {OnOff} from "./components/OnOff/OnOff";
 
 function App() {
-    const [ratingValue, setRatingValue] = useState<RatingValueType>(4)
-    const [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false)
-    const [switchOn, setSwitchOn] = useState<boolean>(false)
+    let [ratingValue,setRatingValue]=useState<RatingType>(0)
+    let [accordionCollapsed, setAccordionCollapsed] = useState(true)
+    let [switchOn, setSwitchOn ] = useState(false)
 
     return (
-        <div>
-            <OnOff switchOn={switchOn} setSwitchOn={setSwitchOn}/>
-            <Rating ratingValue={ratingValue} setRatingValue={setRatingValue}/>
-            <Accordion
-                title={'Кря'}
-                accordionCollapsed={accordionCollapsed}
-                setAccordionCollapsed={setAccordionCollapsed}
-            />
+        <div className="App">
+            {/*<PageTitle title={'This is APP component'}/>*/}
+
+            {/*<Rating value={ratingValue} onClick={setRatingValue}/>*/}
+            {/*<UncontrolledRating />*/}
+
+            {/*<Accordion*/}
+            {/*    titleValue={'Menu'}*/}
+            {/*    collapsed={accordionCollapsed}*/}
+            {/*    onClick={()=>{setAccordionCollapsed(!accordionCollapsed)}}/>*/}
+            {/*<UncontrolledAccordion titleValue={'Menu'} />*/}
+
+
+
+            {/*<OnOff on={switchOn} onClick={setSwitchOn}/>*/}
+            <UncontrolledOnOff onClick={setSwitchOn}/>{switchOn.toString()}
         </div>
-    )
+    );
+}
+
+type PageTitlePropsType={
+    title:string
+}
+const PageTitle = (props:PageTitlePropsType) => {
+    console.log('AppTitle rendering')
+    return <h1>{props.title}</h1>
 }
 
 export default App;
