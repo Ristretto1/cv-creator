5b41ece8c1a2ff4c4730f3f2ee9885258d0a41ed
Antonov Kirill <antonovkir2@gmail.com>
2022-05-17 12:32:07 +0500

    add keyPress actions

--- src/components/Select/SelectDim.tsx
+++ src/components/Select/SelectDim.tsx
@@ -32,21 +32,24 @@ export const SelectDim = (props: SelectPropsType) => {
     }
 
     const onKeyUp = (e: KeyboardEvent<HTMLDivElement>) => {
-        for (let i = 0; i < props.items.length; i++) {
-            if (props.items[i].value === hoveredItemValue) {
-                if (e.key === 'ArrowDown') {
-                    if (props.items[i + 1]) {
-                        props.onChange(props.items[i + 1].value)
-                        break
-                    }
-                }
-                if (e.key === 'ArrowUp') {
-                    if (props.items[i - 1]) {
-                        props.onChange(props.items[i - 1].value)
-                        break
+        if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+            for (let i = 0; i < props.items.length; i++) {
+                if (props.items[i].value === hoveredItemValue) {
+                    const pretendentEl = e.key === 'ArrowDown' ? props.items[i + 1] : props.items[i - 1]
+                    if (pretendentEl) {
+                        if (pretendentEl) {
+                            props.onChange(pretendentEl.value)
+                            return
+                        }
                     }
                 }
             }
+            if (selectedItem) {
+                props.onChange(props.items[0].value)
+            }
+        }
+        if (e.key === 'Enter' || e.key === 'Escape') {
+            setActive(false)
         }
     }
 
